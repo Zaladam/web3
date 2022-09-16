deeaac485e9f4f13f5a69aef6e8b2e31e221bb8a
adam.elmrabet <adam.elmrabet@student.vinci.be>
2022-09-16 23:35:15 +0200

    1.1-1.2 : CSS style

--- exercices/1.1-1.2/src/components/App/App.jsx
+++ exercices/1.1-1.2/src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import Content from "components/Content/Content"
 import Header from "components/Header/Header"
 import Total from "components/Total/Total"
-
+import './App.css'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -12,7 +12,7 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
   return (
-    <div>
+    <div className="test">
       <Header course= {course} />
       <Content parts={[part1,part2,part3]} exercices={[exercises1,exercises2,exercises3]}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
