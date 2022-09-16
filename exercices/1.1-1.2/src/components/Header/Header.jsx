8399f7f8d5a853417ff2e34dca2dcf577a6b5662
adam.elmrabet <adam.elmrabet@student.vinci.be>
2022-09-16 23:53:36 +0200

    1.1-1.2 : import image

--- exercices/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
+import logo from './vinci.png';
+import './Header.css'
+
 const Header = (props)=> {
     return(
-      <div>
+      <div className='Header'>
+        <img src={logo} alt="" srcset="" />
         <h1>{props.course}</h1>
       </div>
     )
