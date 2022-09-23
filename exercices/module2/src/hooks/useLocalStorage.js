75f99d5bd6210f45151632fb9e3ccc1014e4792f
adam.elmrabet <adam.elmrabet@student.vinci.be>
2022-09-23 09:11:48 +0200

    module2 : custom hook (modif 1)

--- exercices/module2/src/hooks/useLocalStorage.js
+++ exercices/module2/src/hooks/useLocalStorage.js
@@ -2,18 +2,14 @@ import {useState} from "react";
 
 const useLocalStorage = (key,initialValue) =>{
     const [counter,setCounter] = useState(JSON.parse(localStorage.getItem(key)) ?? initialValue)
-    let value = JSON.parse(localStorage.getItem(key))
-    if(value === null){
-        value = counter
-        localStorage.setItem(key, JSON.stringify(value))
-    }
+
+
     const setValue = (newValue) =>{
-        value = newValue
-        setCounter(value)
-        localStorage.setItem(key, JSON.stringify(value))
+        setCounter(newValue)
+        localStorage.setItem(key, JSON.stringify(newValue))
     }
     return[
-        value,
+        counter,
         setValue
     ]
 
